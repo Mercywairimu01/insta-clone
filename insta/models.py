5122b878214282ac715e4854912c2882e85c6626
Mercywairimu01 <mercy.mambui@student.moringaschool.com>
2022-06-04 12:21:43 +0300

    add methods to the class models

--- insta/models.py
+++ insta/models.py
@@ -14,17 +14,69 @@ class Image(models.Model):
     created_at = models.DateTimeField(auto_now_add = True)
     profile = models.ForeignKey('Profile', on_delete=models.CASCADE)
     likes = models.ManyToManyField(User, blank=True)
+    
+    def save_image(self):
+        ''' 
+        method to save an image  instance 
+        '''
+        self.save()
+
+    def delete_image(self):
+        '''
+        method to delete an image instance 
+        '''
+        self.delete()
+        
+    def total_likes(self):
+        return self.likes.count()    
+
+    
    
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     profile_pic = models.ImageField(default='default.jpg', upload_to='images/')
     bio = models.TextField(max_length=500, blank=True, default=f'Cool')
+    
+    def save_profile(self):
+        '''
+        method to save a user's profile 
+        '''
+        self.save()
+
+    def delete_profile(self):
+        '''
+        method to delete a user's profile 
+        '''
+        self.delete()
+
+    def update_bio(self, new_bio):
+        ''' 
+        method to update a users profile bio 
+        '''
+        self.bio = new_bio
+        self.save()
+
+    def update_image(self, user_id, new_image):
+        ''' 
+        method to update a user's profile image 
+        '''
+        user = User.objects.get(id=user_id)
+        self.photo = new_image
+        self.save()
+    
 
 class Comment(models.Model):
     comment = models.TextField()
     image = models.ForeignKey(Image, on_delete=models.CASCADE)
     user = models.ForeignKey(Profile, on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True, null=True)
+    
+    def __str__(self):
+        return f'Comment by {self.user}'
+
+    def save_comments(self):
+        ''' method to save comment instance '''
+        self.save()
 
 class Follow(models.Model):
     
