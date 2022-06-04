15de0aba4eccde9e33bc763c76de6ae4fcd2ae68
Mercywairimu01 <mercy.mambui@student.moringaschool.com>
2022-06-04 12:13:20 +0300

    add comments and  follow  models

--- insta/models.py
+++ insta/models.py
@@ -1,6 +1,7 @@
 
+from email.mime import image
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
 class Image(models.Model):
     '''
@@ -12,7 +13,26 @@ class Image(models.Model):
     image_caption = models.CharField(max_length = 60, blank = True)
     created_at = models.DateTimeField(auto_now_add = True)
     profile = models.ForeignKey('Profile', on_delete=models.CASCADE)
+    likes = models.ManyToManyField(User, blank=True)
    
 class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
     profile_pic = models.ImageField(default='default.jpg', upload_to='images/')
     bio = models.TextField(max_length=500, blank=True, default=f'Cool')
+
+class Comment(models.Model):
+    comment = models.TextField()
+    image = models.ForeignKey(Image, on_delete=models.CASCADE)
+    user = models.ForeignKey(Profile, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True, null=True)
+
+class Follow(models.Model):
+    
+    ''' 
+    model for user relations following
+    '''
+    follower = models.ForeignKey('Profile', related_name='following', on_delete=models.CASCADE)
+    followed = models.ForeignKey('Profile', related_name='followers', on_delete=models.CASCADE)
+    
+    def __str__(self):
+        return '{self.follower} follows {self.followed}'
