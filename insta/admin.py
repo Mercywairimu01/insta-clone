f9e84f0622dcd86ab953556e13de12b50e0f451e
Mercywairimu01 <mercy.mambui@student.moringaschool.com>
2022-06-08 00:11:34 +0300

    registered the models

--- insta/admin.py
+++ insta/admin.py
@@ -1,8 +1,5 @@
 from django.contrib import admin
-from .models import Image, Profile, Comment, Follow
+from .models import Post,Profile
 # Register your models here.
-
-admin.site.register(Image)
-admin.site.register(Comment)
+admin.site.register(Post)
 admin.site.register(Profile)
-admin.site.register(Follow)
