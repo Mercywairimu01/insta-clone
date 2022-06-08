844206eeb9510315e8793093585e5f1ceda2c89a
Mercywairimu01 <mercy.mambui@student.moringaschool.com>
2022-06-08 08:29:54 +0300

    fixed issue with login

--- insta/tests.py
+++ insta/tests.py
@@ -1,3 +1,52 @@
 from django.test import TestCase
+from .models import Profile, Image
 
 # Create your tests here.
+class ProfileTestClass(TestCase):
+    # Set up method
+    def setUp(self):
+        self.profile = Profile(image='')
+        
+    # Testing instance
+    def test_instance(self):
+        self.assertTrue(isinstance(self.profile, Profile))
+        
+    # Testing Save Method
+    def test_save_method(self):
+        self.profile.save_profile()
+        profiles = Profile.objects.all()
+        self.assertEqual(len(profiles), 1)
+    
+    # Testing Delete Method
+    def test_delete_method(self):
+        self.profile.save_profile()
+        self.profile.delete_profile()
+        profiles = Profile.objects.all()
+        self.assertEqual(len(profiles), 0)
+
+
+    
+class PostTestClass(TestCase):
+    # Set up method
+    def setUp(self):
+        self.profile = Profile(image='')
+        self.profile.save_profile()
+        
+        self.image = self.profile.image_set.create(image='https://res.cloudinary.com/dzqbzqjqw/image/upload/v1559098981/image_qjqjqj.jpg', name='image', captions='This is a caption', likes=0, comments='This is a comment')
+        
+    # Testing instance
+    def test_instance(self):
+         self.assertTrue(isinstance(self.image, Image))
+         
+    # Testing Save Method
+    def test_save_method(self):       
+        self.image.save_image()
+        images = Image.objects.all()
+        self.assertEqual(len(images), 1)
+        
+    # Testing Delete Method
+    def test_delete_method(self):       
+        self.image.save_image()
+        self.image.delete_image()
+        images = Image.objects.all()
+        self.assertEqual(len(images), 0)
